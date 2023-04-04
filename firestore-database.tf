16cdcdaea06ab00af068d23fb1d96e5b4910d0ea
mtariga266 <126924464+mtariga266@users.noreply.github.com>
2023-04-03 23:41:58 -0400

    Update firestore-database.tf

--- firestore-database.tf
+++ firestore-database.tf
@@ -1,6 +1,6 @@
 resource "google_firestore_database" "database" {
   project = "my-second-project-381922"
   name = "my-firestoredb"
-  location_id = "nam5" type="FIRESTORE_NATIVE"
-  
-}
+  location_id = "nam5"
+  type="FIRESTORE_NATIVE"
+  }
